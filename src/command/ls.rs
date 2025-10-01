2a98952c8a306da435fb3273dfd775cae3839920
Ilyass Afriad <ilyassafriad@gmail.com>
2025-10-01 20:55:00 +0100

    ls command parse flags

--- src/command/ls.rs
+++ src/command/ls.rs
@@ -1,11 +1,59 @@
-pub fn ls(args: &[&str]) -> Result<(), String> {
-    let paths = std::fs::read_dir(".").map_err(|e| e.to_string())?;
-    
-    for path in paths {
-        let entry = path.map_err(|e| e.to_string())?;
-        let file_name = entry.file_name();
-        println!("{}", file_name.to_string_lossy());
+use std::io;
+
+
+pub fn ls(args: &[&str]) -> io::Result<()> {
+    let mut paths = Vec::new();
+    let mut flags = Flags::new();
+
+    parse(args, &mut flags, &mut paths)?;
+
+    if paths.is_empty() {
+        paths.push(".".to_string());
     }
+
+    println!("this is the paths: {:?}", paths);
+    println!("this is the flags: {:?}", flags);
     
+    Ok(())
+}
+
+
+
+
+
+#[derive(Debug)]
+struct Flags {
+    a: bool,
+    F: bool,
+    l: bool,
+}
+
+impl Flags {
+    fn new() -> Self {
+        Self { a: false, F: false, l: false }
+    }
+}
+
+fn parse(args: &[&str], flg: &mut Flags, paths: &mut Vec<String>) -> io::Result<()> {
+    for arg in args {
+        if arg.starts_with('-') && arg.len() > 1 {
+            let arg_flags = arg.trim_start_matches('-');
+            for f in arg_flags.chars() {
+                match f {
+                    'F' => flg.F = true,
+                    'l' => flg.l = true,
+                    'a' => flg.a = true,
+                    _ => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidInput, 
+                            format!("ls: invalid option -- '{}'", f)
+                        ));
+                    }
+                }
+            }
+        } else {
+            paths.push(arg.to_string());
+        }
+    }
     Ok(())
 }
